211f49de73f937dcca47cd6e6b11a4b677292350
Orchid23 <calliet0715@gmail.com>
2020-02-23 12:50:03 -0600

    Removal of TODO comment

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,4 +1,4 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
 const employee = require("./Employee");
 
 class Engineer extends employee {
