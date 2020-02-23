7d0ef13f60ccbc509c9a037e6cf73550bf2a8b9d
Orchid23 <calliet0715@gmail.com>
2020-02-23 12:44:22 -0600

    Setup code-addition of constructor and role function

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,34 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const employee = require("./Employee");
+
+class Manager extends employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+    getOfficeNumber(){
+        return this.officeNumber;
+    }
+
+    getRole() {
+        return "Manager";
+    }
+}
+
+
+
+module.exports = Manager;
+
+
+//.then()
+
+/* 1. call manager prompt 
+2. .then class list prompt
+    if select engineer 
+       call engineer prompt (inquire)
+
+   else if select Intern
+     call intern prompt (inquire)
+
+     else generate HTML
+ */
