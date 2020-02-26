b2b102e613418b78d228156339bb22f758b3d996
Orchid23 <calliet0715@gmail.com>
2020-02-25 18:43:35 -0600

    Changing schools to college

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -4,7 +4,7 @@ const employee = require("./Employee");
 class Intern extends employee {
     constructor(name, id, email, college) {
         super(name, id, email); //inherited from employee
-        this.school = college; //unique for intern
+        this.college = college; //unique for intern
         
         //generated if user does not supply answer to the questions
         if (!name) {
@@ -13,7 +13,7 @@ class Intern extends employee {
         if (!email) {
             throw new Error("Please enter the employee's email")
         }
-        if (!school) {
+        if (!college) {
             throw new Error("Please enter the college the intern is attending.");
         }
     }
