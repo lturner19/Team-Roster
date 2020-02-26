7148795dfecba4fb55092e373fa0b830dc9aa5a4
Orchid23 <calliet0715@gmail.com>
2020-02-26 07:13:45 -0600

    Addition of background color to heading

--- Develop/templates/intern.js
+++ Develop/templates/intern.js
@@ -2,7 +2,7 @@ function intern (data){
 return `
 
 <div class="card employee-card">
-    <div class="card-header">
+    <div class="card-header bg-warning">
         <h2 class="card-title">${data.name}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${data.getRole()}</h3>
     </div>
@@ -10,7 +10,7 @@ return `
         <ul class="list-group">
             <li class="list-group-item">ID: ${data.id}</li>
             <li class="list-group-item">Email: <a href="mailto:${data.email}">${data.email}</a></li>
-            <li class="list-group-item">School: ${data.college}</li>
+            <li class="list-group-item">College: ${data.college}</li>
         </ul>
     </div>
 </div>
