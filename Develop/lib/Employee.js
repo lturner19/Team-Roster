7672e633a1cd91e6514c9f5c0fd45ca00486d72a
Orchid23 <calliet0715@gmail.com>
2020-02-26 11:00:26 -0600

    Final comments

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,6 +1,7 @@
-//creating "parent"
-//the following constructors will be inherited by the "children"
+//creating "parent" file
+
 class Employee {
+    //The following will be inherited by following: manager, engineer, intern
     constructor(name, id, email) {
         this.name = name;
         this.id = id;
@@ -16,7 +17,7 @@ class Employee {
     getEmail() {
         return this.email;
     }
-    //default employee role
+    //default role
     getRole() {
         return "Employee";
     }
